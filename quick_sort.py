387a01c233658b5ef8117a842bb2095cf0efcec5
GaryLu <gary.luwenkai@gmail.com>
2024-06-29 01:10:02 +0800

    add quick sort

---
+++ quick_sort.py
@@ -0,0 +1,28 @@
+def _quick_sort(data, left, right):
+    if left < right:
+        mid = partition(data, left, right)
+        _quick_sort(data, left, mid-1)
+        _quick_sort(data, mid+1, right)
+
+def partition(data, left, right):
+    cur = data[left]
+    while left < right:
+        while left < right and data[right] > cur:
+            right -= 1
+
+        data[left] = data[right]
+
+        while left < right and data[left] < cur:
+            left += 1
+
+        data[right] = data[left]
+    data[left] = cur
+    return left
+
+def quick_sort(data):
+    _quick_sort(data, 0, len(data)-1)
+
+if __name__ == '__main__':
+    li = [4,1,3,7,5,2,6]
+    quick_sort(li)
+    print(li)
